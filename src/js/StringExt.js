b415a977586b207f82581c83d54f1d617668cede
Ubuntu <ubuntu@sosol-test.perseids.org>
2014-01-15 19:25:19 +0000

    Added params() method to parse GET parameters.

--- src/js/StringExt.js
+++ src/js/StringExt.js
@@ -1,6 +1,29 @@
 /**
  * Custom String methods
  */
+
+/**
+ * Remove unnecessary whitespace from a string
+ */
 String.prototype.smoosh = function() {
 	return this.replace(/(\r\n+|\n+|\r+|\t+)/gm,'');
+}
+
+/**
+ * Turn a string with HTTP GET style parameters to an object
+ *
+ * @return { obj } A collection of keys and values
+ */
+String.prototype.params = function() {
+	var arr = this.split('?');
+	var get = arr[1];
+	arr = get.split('&');
+	var out = {};
+	for ( var i=0, ii=arr.length; i<ii; i++ ) {
+		if ( arr[i] != undefined ) {
+			var pair = arr[i].split('=');
+			out[ pair[0] ] = pair[1];
+		}
+	}
+	return out;
 }
