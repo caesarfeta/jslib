37d8ef0298089ea2f5105df1a5ec93a6adec6ac6
caesarfeta <adamtavares@gmail.com>
2014-01-23 17:38:17 -0500

    Added sentences() method to StringExt.  It's not perfect but it's not bad.

--- src/js/StringExt.js
+++ src/js/StringExt.js
@@ -1,8 +1,4 @@
 /**
- * Custom String methods
- */
-
-/**
  * Remove newlines and tabs
  */
 String.prototype.smoosh = function() {
@@ -14,29 +10,26 @@ String.prototype.smoosh = function() {
  *
  * @parm {string} _sub : The search string
  * @param {boolean} _overlap : Optional. Default: false
+ * @return {int} : The count
  */
 String.prototype.occurs = function( _search, _overlap ) {
 	var string = this;
-	
 	//------------------------------------------------------------
 	//  If _search is null just return a char count
 	//------------------------------------------------------------
 	if ( _search == undefined ) {
 		return string.length;
 	}
-	
 	//------------------------------------------------------------
 	//  Make sure _search is a string
 	//------------------------------------------------------------
 	_search+="";
-	
 	//------------------------------------------------------------
 	//  If no search term is past just return a character count
 	//------------------------------------------------------------
 	if ( _search.length <= 0 ) {
 		return string.length;
 	}
-	
 	//------------------------------------------------------------
 	//  Otherwise start counting.
 	//------------------------------------------------------------
@@ -71,6 +64,69 @@ String.prototype.params = function() {
 			var pair = arr[i].split('=');
 			out[ pair[0] ] = pair[1];
 		}
+	}
+	return out;
+}
+
+/*
+ * Divide text into an array of individual sentences
+ * This is English-centric.  Forgive me.
+ *
+ * @return { array } An array of sentences
+ */
+String.prototype.sentences = function() {
+	var check = this.match( /[^\.!\?]+[\.!\?]+/g );
+	
+	//------------------------------------------------------------
+	//  Make sure characters aren't used for purposes other than
+	//  sentences.
+	//------------------------------------------------------------
+	var vowels = [ 'a','e','i','o','u','y' ];
+	var out = [];
+	var carry = '';
+	for ( var i=0; i<check.length; i++ ) {
+		//------------------------------------------------------------
+		//  Clean up.
+		//------------------------------------------------------------
+		var strCheck = carry + check[i];
+		carry = '';
+		//------------------------------------------------------------
+		//  Check for the existence of a vowel, so we aren't
+		//  accidentally thinking part of an abbreviation is its
+		//  own sentence.
+		//------------------------------------------------------------
+		var merge = true;
+		for ( var j=0; j<vowels.length; j++ ) {
+			if ( strCheck.indexOf( vowels[j] ) != -1 ) {
+				merge = false;
+				break;
+			}
+		}
+		//------------------------------------------------------------
+		//  Also check for a capital letter.  Most sentences have
+		//  those too.
+		//------------------------------------------------------------
+		if ( !( /[A-Z]/.test( strCheck ) ) ) {
+			merge = true;
+		}
+		//------------------------------------------------------------
+		//  If no vowel exists in the sentence you're probably
+		//  dealing with an abbreviation.  Merge with last sentence.  
+		//------------------------------------------------------------
+		if ( merge ) {
+			if ( out.length > 0 ) {
+				out[ out.length-1 ] += strCheck;
+			}
+			else {
+				carry = strCheck;
+			}
+			continue;
+		}
+		
+		//------------------------------------------------------------
+		//  Prepare output.
+		//------------------------------------------------------------
+		out.push( strCheck.smoosh().trim() );
 	}
 	return out;
 }
--- test/unit/StringExt.js
+++ test/unit/StringExt.js
@@ -16,21 +16,38 @@ test( "occurs-0", function() {
 	ok( count == 7 );
 });
 
-test( "occurs-0", function() {
+test( "occurs-1", function() {
 	var string = "1234567890";
 	var count = string.occurs();
-	console.log( count );
 	ok( count == 10 );
 });
 
-test( "occurs-0", function() {
-	var string = "aaa"
+test( "occurs-2", function() {
+	var string = "aaa";
 	var count = string.occurs( 'aa' );
 	ok( count == 1 );
 });
 
-test( "occurs-0", function() {
-	var string = "aaa"
+test( "occurs-3", function() {
+	var string = "aaa";
 	var count = string.occurs( 'aa', true );
 	ok( count == 2 );
+});
+
+test( "sentences-0", function() {
+	var string = "Hi! How are you? Can we talk?? Please?";
+	var sent = string.sentences();
+	ok( sent.length == 4 );
+});
+
+test( "sentences-1", function() {
+	var string = "Did you enjoy living in Washington, D.C.?";
+	var sent = string.sentences();
+	ok( sent.length == 1 );
+});
+
+test( "sentences-2", function() {
+	var string = "Dr. Espinoza arrived from Washington, D.C., at 6 p.m.";
+	var sent = string.sentences();
+	ok( sent.length == 1 );
 });
