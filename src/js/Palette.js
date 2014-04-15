8f5c6ad3368145715541547892545c85eff891ee
caesarfeta <adamtavares@gmail.com>
2014-04-15 14:22:03 -0400

    Added 'candy' palette.

--- src/js/Palette.js
+++ src/js/Palette.js
@@ -2,6 +2,8 @@
  * Palettes are collections of colors.
  * Dependencies:
  * 		Culuh.js
+ *
+ * @param { string } _name The name of the palette.
  */
 function Palette( _name ) {
 	this.palette = [];
@@ -16,17 +18,50 @@ function Palette( _name ) {
 Palette.prototype.load = function( _name ) {
 	switch ( _name ) {
 		case 'grayscale':
-			this.add([ '#000000', '#333333', '#666666', '#999999', '#CCCCCC' ]);
+			this.add([ 
+				'#000000', 
+				'#333333', 
+				'#666666', 
+				'#999999', 
+				'#CCCCCC', 
+				'#FFFFFF' 
+			]);
 			break;
 		case 'primary':
-			this.add([ '#FF0000', '#00FF00', '#0000FF' ]);
+			this.add([ 
+				'#FF0000', // red
+				'#00FF00', // green
+				'#0000FF'  // blue
+			]);
 			break;
 		case 'secondary':
-			this.add([ '#FFFF00', '#00FFFF', '#FF00FF' ]);
+			this.add([ 
+				'#00FFFF', // cyan
+				'#FF00FF', // magenta
+				'#FFFF00'  // yellow
+			]);
 			break;
+		case 'candy':
+			this.add([
+				'#F4DDBE', // coffee milk
+				'#465A95', // sunset purple
+				'#10CCD5', // blue raspberry
+				'#FD8471', // peach skin
+				'#88D499'  // blue grass
+			]);
 		case undefined:
-			this.add([ '#000000', '#FFFFFF' ]);
+			this.add([ 
+				'#000000', // black
+				'#FFFFFF'  // white
+			]);
 	}
+}
+
+/**
+ * Reset the palette
+ */
+Palette.prototype.reset = function() {
+	this.palette = [];
 }
 
 /**
