7fbf4234cb4aaf8b1ddb4f72429bc70dcd311d22
caesarfeta <adamtavares@gmail.com>
2014-07-18 09:49:04 -0400

    Improved Contemplate allowing for features to be ignored.

--- src/js/Contemplate.js
+++ src/js/Contemplate.js
@@ -6,8 +6,23 @@ function Contemplate() {
 	var grabber = jQuery('*[data-contemplate="go"]');
 	grabber.each( function(_i){
 		var me = jQuery( this );
-		var params = me.attr('data-params').shellArgs();
-		var map = Replacers[ me.attr('data-replacer')]( params, _i+1 );
+		//------------------------------------------------------------
+		//  Grab the parameters if they exist.
+		//------------------------------------------------------------
+		var params = [];
+		if ( params = me.attr('data-params') ) {
+			params = params.shellArgs();
+		}
+		//------------------------------------------------------------
+		//  If a Replacer function exists then pass the params to it.
+		//------------------------------------------------------------
+		var map = {};
+		if ( map = me.attr('data-replacer') ) {
+			map = Replacers[ map ]( params, _i+1 );
+		}
+		//------------------------------------------------------------
+		//  Grab the template and do it again.
+		//------------------------------------------------------------
 		jQuery.ajax({
 			url: me.attr('data-file'),
 			type: 'GET',
