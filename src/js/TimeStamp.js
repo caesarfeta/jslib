bc6b29920d478dc018abed8b9555d17e9c31a0fa
caesarfeta <adamtavares@gmail.com>
2014-11-21 17:08:23 -0500

    Added xsd method to TimeStamp.js

--- src/js/TimeStamp.js
+++ src/js/TimeStamp.js
@@ -1,16 +1,46 @@
-/**
- * Get a quality timestamp
- * @requires datejs ../third_party/datejs.date.js [ http://www.datejs.com/ ]
- */
-function TimeStamp() {}
 
-/**
- * Return a timestamp with a UTC offset
- *
- * @param { boolean } _milli include milliseconds
- * @return { string } timestamp with UTC offset
- */
-TimeStamp.prototype.withUtc = function( _milli ) {
+// Get a quality timestamp
+// @requires datejs ../third_party/datejs.date.js [ http://www.datejs.com/ ]
+// @requires StringExt.js
+
+function TimeStamp(){}
+
+TimeStamp.prototype.xsd = function(){
+	var d = new Date();
+	var yyyy = d.getFullYear();
+	var mm = ('0' + (d.getMonth()+1)).slice(-2);
+	var dd = ('0' + d.getDate()).slice(-2);
+	var hh = d.getHours();
+	var min = ('0' + d.getMinutes()).slice(-2);
+	var sec = ('0' + d.getSeconds()).slice(-2);
+	var diff = d.getTimezoneOffset();
+	
+	var time = yyyy+'-'+mm+'-'+dd+' '+hh+":"+min+":"+sec+' ';
+	if ( diff > 0 ) {
+		time = time+"+"+this.pad( 4, diff );
+	}
+	else {
+		time = time+"-"+this.pad( 4, diff );
+	}
+	return time;
+}
+
+// Pad a number
+
+TimeStamp.prototype.pad = function( n, num ){
+	if ( num <= parseInt('9'.repeat(n) )){ 
+		num = ('0'.repeat(n-1)+num).slice(-n); 
+	}
+	  return num;
+}
+
+
+// Return a timestamp with a UTC offset
+
+// @param { boolean } _milli include milliseconds
+// @return { string } timestamp with UTC offset
+
+TimeStamp.prototype.withUtc = function( _milli ){
 	var d = new Date();
 	var yyyy = d.getFullYear();
 	var mm = ('0' + (d.getMonth()+1)).slice(-2);
@@ -21,9 +51,8 @@ TimeStamp.prototype.withUtc = function( _milli ) {
 	var mil = ('0' + d.getMilliseconds()).slice(-3);
 	var diff = d.getTimezoneOffset();
 	
-	//------------------------------------------------------------
 	//  Include milliseconds?
-	//------------------------------------------------------------
+
 	var time = '';
 	if ( _milli ) {
 		time = yyyy+'-'+mm+'-'+dd+'T'+hh+":"+min+":"+sec+":"+mil+"UTC";
@@ -32,9 +61,8 @@ TimeStamp.prototype.withUtc = function( _milli ) {
 		time = yyyy+'-'+mm+'-'+dd+'T'+hh+":"+min+":"+sec+"UTC";		
 	}
 	
-	//------------------------------------------------------------
 	//  Get the timezone offset
-	//------------------------------------------------------------
+	
 	if ( diff > 0 ) {
 		time = time+"+"+diff;
 	}
@@ -44,12 +72,12 @@ TimeStamp.prototype.withUtc = function( _milli ) {
 	return time;
 }
 
-/**
- * Return the time of day
- *
- * @return { String } Easily understood time of day
- */
-TimeStamp.prototype.timeOfDay = function() {
+
+// Return the time of day
+   
+// @return { String } Easily understood time of day
+
+TimeStamp.prototype.timeOfDay = function(){
 	var d = new Date();
 	var hh = d.getHours();
 	var min = ('0' + d.getMinutes()).slice(-2);
@@ -62,30 +90,30 @@ TimeStamp.prototype.timeOfDay = function() {
 	return hh+":"+min+" "+dd;
 }
 
-/**
- * Return unix time
- *
- * @return { int } unix time
- */
-TimeStamp.prototype.unix = function() {
+
+// Return unix time
+
+// @return { int } unix time
+
+TimeStamp.prototype.unix = function(){
 	return new Date().getTime();
 }
 
-/**
- * Return millisecond unix time from UTC string
- *
- * @param { string } _string timestamp with UTC offset
- * @return { int } unix time
- */
-TimeStamp.prototype.toUnix = function( _string ) {
-	//------------------------------------------------------------
+
+// Return millisecond unix time from UTC string
+   
+// @param { string } _string timestamp with UTC offset
+// @return { int } unix time
+
+TimeStamp.prototype.toUnix = function( _string ){
+	
 	// Kill the UTC offset
-	//------------------------------------------------------------
+
 	var cleanTime = _string.replace( /UTC.*/, '' );
 	var milli = 0;
-	//------------------------------------------------------------
+
 	// Grab the milliseconds if they exist
-	//------------------------------------------------------------
+
 	if ( cleanTime.match( /:\d{3}/ ) ) {
 		milli = cleanTime.slice( -4 );
 		cleanTime = cleanTime.replace( /:\d+$/, '' );
